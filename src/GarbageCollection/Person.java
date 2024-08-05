ee914238865b88e116981fb56bef4474ad194927
MadanB98 <madanbura56@gmail.com>
2024-08-05 14:55:14 +0530

    Adding >>

---
+++ src/GarbageCollection/Person.java
@@ -0,0 +1,46 @@
+package GarbageCollection;
+
+class Test {
+
+    public static void main(String args[]) {
+
+        Person p = new Person("Madan");
+        Person p1 = new Person("Madan");
+
+        p = null;
+        p1 = null;
+
+        // Request garbage collection
+        System.gc();
+
+        // Wait for a bit to give the garbage collector time to run
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("End of main method.");
+
+    }
+}
+
+
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    //Finalize method - it is called by garbage collector just before the object gets
+    // garbage collected
+    //From Java 9 Finalize method has been deprecated
+    //It is recommended to use `try-with resources`
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println(this.name + " is being garbage collected.");
+    }
+
+}
---
+++ src/GarbageCollection/Try_With_Resource_Example.java
@@ -0,0 +1,49 @@
+package GarbageCollection;
+
+//The AutoCloseable interface in Java is part of the java.lang package
+// and provides a way to ensure that resources are closed automatically when they are no longer needed.
+
+
+//Try_With Resource
+//it is mechanism used to handle resources which are getting closed automatically after end this block
+class Test_{
+    public static void main(String[] args) {
+
+        try(Try_With_Resource_Example t = new Try_With_Resource_Example("Database_File");
+        Try_With_Resource_Example t1 = new Try_With_Resource_Example("JSON_File")){
+
+        t.doingOperation();
+        t1.doingOperation();
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("End of the method >> ");
+
+
+
+
+    }
+}
+
+
+
+public class Try_With_Resource_Example implements AutoCloseable {
+
+    private String resource;
+
+    public Try_With_Resource_Example(String resource) {
+        this.resource = resource;
+        System.out.println(resource+" is opened >> ");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("closing this "+resource+" >>");
+    }
+
+    public void doingOperation(){
+        System.out.println(resource+" is doing operation >>> ");
+    }
+}
